e767f5ce42ab0c93e47a09d4b6e75bda3d75c418
Tomasz <tomaszcyganek@poczta.fm>
2019-08-04 11:09:51 +0200

    Seleniumkoszyk - zmykanie informacji o cisteczkach

--- src/test/java/pl/sda/kurs/DodajDoKoszyka.java
+++ src/test/java/pl/sda/kurs/DodajDoKoszyka.java
@@ -4,8 +4,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
@@ -27,7 +30,15 @@ public class DodajDoKoszyka {
     public void ilośćRówna1(){
         przegladarka.get("https://www.ocado.com/browse");
         List<WebElement> produkty = przegladarka.findElements(By.cssSelector(".fops-item"));
+
+        przegladarka.findElement(By.cssSelector(".cd-cookieDisclaimer__close")).click();
+        WebDriverWait czekaj = new WebDriverWait(przegladarka,10);
+
+        czekaj.until(ExpectedConditions.elementToBeClickable(produkty.get(3).findElement(By.cssSelector(".fop-btn-add"))));
+
         produkty.get(3).findElement(By.cssSelector(".fop-btn-add")).click();
+
+
 
     }
 
